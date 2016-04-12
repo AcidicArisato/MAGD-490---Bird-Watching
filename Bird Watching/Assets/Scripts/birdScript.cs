215953e940b8cb583b2258b387c7364254de4a62
Brandon Hanabarger <animalxingguard@gmail.com>
2016-04-11 18:10:46 -0700

    Removed ghost OVR files, added some notes
    Notes pertain to achievements

---
+++ Bird Watching/Assets/Scripts/birdScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class birdScript : MonoBehaviour {
+
+	public float timer;
+
+	public Text timeDisplay;
+
+	public Transform bluejayPrefab_1;
+//	public Transform robinPrefab_1;
+//	public Transform sparrowPrefab_1;
+
+	private bool hasSpawnedBluejay_1 = false;
+//	private bool hasSpawnedRobin_1 = false;
+//	private bool hasSpawnedSparrow_1 = false;
+
+	public Vector3 bluejayPos_1;
+//	public Vector3 robinPos_1;
+//	public Vector3 sparrowPos_1;
+	
+
+	void Update () {
+	
+		//Used to keep track of time elapsed
+		timer -= Time.deltaTime;
+		timeDisplay.text = timer.ToString ("000");
+
+		//Used to place birds into scene at specific times
+		if (timer <= 110 && !hasSpawnedBluejay_1) {
+			Instantiate (bluejayPrefab_1, bluejayPos_1, Quaternion.identity);
+			hasSpawnedBluejay_1 = true;
+		}
+
+		/*if (timer <= 105 && !hasSpawnedRobin_1) {
+			Instantiate (robinPrefab_1, robinPos_1, Quaternion.identity);
+			hasSpawnedRobin_1 = true;
+		}*/
+
+		/*if (timer <= 100 && !hasSpawnedSparrow_1) {
+			Instantiate (sparrowPrefab_1, sparrowPos_1, Quaternion.identity);
+			hasSpawnedSparrow_1 = true;
+		}*/
+
+	}
+}
---
+++ Bird Watching/Assets/Scripts/bluejay.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bluejay : MonoBehaviour {
+
+	private Vector3 startPos;
+	private Vector3 endPos = new Vector3(-2, 2, -2);
+	private Vector3 nextPos_1 = new Vector3(-2, 4, 2);
+	private Vector3 killPos = new Vector3 (-2, 4, 2);
+	private Vector3 lastPos;
+
+	private float timer_1;
+	private float timer_2;
+
+	private bool en2ndRoute = false;
+
+	void Start () {
+
+		startPos = transform.position;
+	}
+	
+
+	void Update () {
+
+		//Keeps track of time for 1st movement
+		timer_1 += Time.deltaTime;
+
+		//Handles first movement
+		//Modify timer value to increase or decrease time between positions
+		transform.position = Vector3.Lerp (startPos, endPos, (timer_1 * 0.5f));
+
+		//Handles second movement
+		//The difference between the time it takes to complete first movement and the value check below is the time the bird remains at one location
+		if (timer_1 >= 5 && !en2ndRoute) {
+
+			startPos = transform.position;
+			endPos = nextPos_1;
+			en2ndRoute = true;
+		}
+
+		if (en2ndRoute) {
+
+			timer_2 += Time.deltaTime;
+			transform.position = Vector3.Lerp (startPos, endPos, (timer_2 * 0.5f));
+		}
+
+		//Deletes bird after it leaves the scene
+		if (transform.position == killPos)
+			Destroy (gameObject);
+
+		//For animation
+		if (lastPos == transform.position) {
+
+			//set animation to idle
+		} else {
+
+			//set animation to moving
+		}
+	}
+
+	// Checks if bird is moving
+	void lateUpdate () {
+
+		lastPos = transform.position;
+	}
+}
