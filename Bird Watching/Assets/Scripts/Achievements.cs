4d160af727490e9c0843861a482527689e33880d
Eric Peltier <peltierew15@uww.edu>
2016-04-20 18:25:21 -0500

    fixed achievement text display and issues with reocurring achievements

--- Bird Watching/Assets/Scripts/Achievements.cs
+++ Bird Watching/Assets/Scripts/Achievements.cs
@@ -9,22 +9,23 @@ public class Achievements : MonoBehaviour {
 	public Text achievementText;
 	private float time = 0;
 	private Vector3 target = new Vector3(0, -100, 0);
-	private Vector3 hide = new Vector3(0, -450, 0);
+	private Vector3 hide = new Vector3(0, -250, 0);
 
 	// strings for achievement titles
-	private string defaultText = "Achievement!";
-	private string firstBirdFoundText = " First Bird Found";
-	private string threeTypesFoundText = " Three Species Found";
-	private string rareBirdFoundText = " One in a Million";
-	private string ufoFoundText = " Close Encounter";
-	private string bugFoundText = " Debugging";
-	private string fiveBirdsFoundText = " Found 5 Birds";
-	private string tenBirdsFoundText = " Found 10 Birds";
-	private string fifteenBirdsFoundText = " Found 15 Birds";
-	private string nonbirdFoundText = " No Feathers";
-	private string sunFoundText = " Sungazing";
-	private string nothingFoundText = " Missing the Point";
-	private string threeLikeBirdsFoundText = " Birds of a Feather";
+	[HideInInspector]
+	public string defaultText = "Achievement!";
+	public string firstBirdFoundText = " First Bird Found";
+	public string threeTypesFoundText = " Three Species Found";
+	public string rareBirdFoundText = " One in a Million";
+	public string ufoFoundText = " Close Encounter";
+	public string bugFoundText = " Debugging";
+	public string fiveBirdsFoundText = " Found 5 Birds";
+	public string tenBirdsFoundText = " Found 10 Birds";
+	public string fifteenBirdsFoundText = " Found 15 Birds";
+	public string nonbirdFoundText = " No Feathers";
+	public string sunFoundText = " Sungazing";
+	public string nothingFoundText = " Missing the Point";
+	public string threeLikeBirdsFoundText = " Birds of a Feather";
 
 	public bool gotAchievement = false;
 	public bool rising = false;
@@ -41,50 +42,84 @@ public class Achievements : MonoBehaviour {
 				achievementText.text += firstBirdFoundText;
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().fiveBirdsFound == true) {
-			achievementText.text += fiveBirdsFoundText;
+			if (!achievementText.text.Contains(fiveBirdsFoundText))
+				achievementText.text += fiveBirdsFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().tenBirdsFound == true) {
-			achievementText.text += tenBirdsFoundText;
+			if (!achievementText.text.Contains(tenBirdsFoundText))
+				achievementText.text += tenBirdsFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().fifteenBirdsFound == true) {
+			if (!achievementText.text.Contains(fifteenBirdsFoundText))
 			achievementText.text += fifteenBirdsFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().threeTypesFound == true) {
-			achievementText.text += threeTypesFoundText;
+			if (!achievementText.text.Contains(fifteenBirdsFoundText))
+				achievementText.text += threeTypesFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().threeLikeBirdsFound == true) {
-			achievementText.text += threeLikeBirdsFoundText;
+			if (!achievementText.text.Contains(threeLikeBirdsFoundText))
+				achievementText.text += threeLikeBirdsFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().rareBirdFound == true) {
-			achievementText.text += rareBirdFoundText;
+			if (!achievementText.text.Contains(rareBirdFoundText))
+				achievementText.text += rareBirdFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().ufoFound == true) {
-			achievementText.text += ufoFoundText;
+			if (!achievementText.text.Contains(ufoFoundText))
+				achievementText.text += ufoFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().bugFound == true) {
-			achievementText.text += bugFoundText;
+			if (!achievementText.text.Contains(bugFoundText))
+				achievementText.text += bugFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().sunFound == true) {
-			achievementText.text += sunFoundText;
+			if (!achievementText.text.Contains(sunFoundText))
+				achievementText.text += sunFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().nonbirdFound == true) {
-			achievementText.text += nonbirdFoundText;
+			if (!achievementText.text.Contains(nonbirdFoundText))
+				achievementText.text += nonbirdFoundText;
 			
 		}
 		else if (raycastScript.GetComponent<RaycastScript>().nothingFound == true) {
-			achievementText.text += nothingFoundText;
+			if (!achievementText.text.Contains(nothingFoundText))
+				achievementText.text += nothingFoundText;
 		}
 
-		if (raycastScript.GetComponent<RaycastScript>().firstBirdFound == true || raycastScript.GetComponent<RaycastScript>().fiveBirdsFound == true || 
+		if (raycastScript.GetComponent<RaycastScript>().displayFirstBirdFound == true || raycastScript.GetComponent<RaycastScript>().displayFiveBirdsFound == true || 
+			raycastScript.GetComponent<RaycastScript>().displayTenBirdsFound == true || raycastScript.GetComponent<RaycastScript>().displayFifteenBirdsFound == true || 
+			raycastScript.GetComponent<RaycastScript>().displayThreeTypesFound == true || raycastScript.GetComponent<RaycastScript>().displayRareBirdFound == true || 
+			raycastScript.GetComponent<RaycastScript>().displayThreeLikeBirdsFound == true || raycastScript.GetComponent<RaycastScript>().displaySunFound == true ||
+			raycastScript.GetComponent<RaycastScript>().displayBugFound == true || raycastScript.GetComponent<RaycastScript>().displayUfoFound == true || 
+			raycastScript.GetComponent<RaycastScript>().displayNothingFound == true || raycastScript.GetComponent<RaycastScript>().displayNonbirdFound == true)
+		{
+			gotAchievement = true;
+			rising = true;
+			raycastScript.GetComponent<RaycastScript>().displayFirstBirdFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayFiveBirdsFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayTenBirdsFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayFifteenBirdsFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayThreeTypesFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayRareBirdFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayThreeLikeBirdsFound = false;
+			raycastScript.GetComponent<RaycastScript>().displaySunFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayBugFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayUfoFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayNothingFound = false;
+			raycastScript.GetComponent<RaycastScript>().displayNonbirdFound = false;
+		}
+
+		/*if (raycastScript.GetComponent<RaycastScript>().firstBirdFound == true || raycastScript.GetComponent<RaycastScript>().fiveBirdsFound == true || 
 			raycastScript.GetComponent<RaycastScript>().tenBirdsFound == true || raycastScript.GetComponent<RaycastScript>().fifteenBirdsFound == true ||
 			raycastScript.GetComponent<RaycastScript>().threeTypesFound == true || raycastScript.GetComponent<RaycastScript>().threeLikeBirdsFound == true || 
 			raycastScript.GetComponent<RaycastScript>().rareBirdFound == true || raycastScript.GetComponent<RaycastScript>().ufoFound == true ||
@@ -106,17 +141,17 @@ public class Achievements : MonoBehaviour {
 			raycastScript.GetComponent<RaycastScript>().sunFound = false;
 			raycastScript.GetComponent<RaycastScript>().nothingFound = false;
 
-		}
+		}*/
 
 		if (gotAchievement == true) {
 			if (achievementText.transform.localPosition.y < target.y && rising == true) {
 				achievementText.transform.Translate(Vector3.up * 2.5f, Space.World);
-				Debug.Log("rising");
+				//Debug.Log("rising");
 			}
 			else {
 				rising = false;
 				Debug.Log(achievementText.transform.localPosition.y);
-				Debug.Log("lowering");
+				//Debug.Log("lowering");
 				time += Time.deltaTime;
 			
 				// wait for 5 seconds after displaying the achievement, then reset the text and hide it
--- Bird Watching/Assets/Scripts/RaycastScript.cs
+++ Bird Watching/Assets/Scripts/RaycastScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class RaycastScript : MonoBehaviour {
+	private GameObject achievementsScript;
 
 	// bools for unlocking achievements
 	public bool firstBirdFound = false; // find a bird
@@ -18,11 +19,25 @@ public class RaycastScript : MonoBehaviour {
 	public bool nothingFound = false; // find nothing during a play session - "Missing the Point"
 	public bool threeLikeBirdsFound = false; // find three of the same bird - "Birds of a Feather"
 
+	public bool displayFirstBirdFound = false;
+	public bool displayThreeTypesFound = false;
+	public bool displayRareBirdFound = false;
+	public bool displayUfoFound = false;
+	public bool displayBugFound = false;
+	public bool displayFiveBirdsFound = false;
+	public bool displayTenBirdsFound = false;
+	public bool displayFifteenBirdsFound = false;
+	public bool displayNonbirdFound = false;
+	public bool displaySunFound = false;
+	public bool displayNothingFound = false;
+	public bool displayThreeLikeBirdsFound = false;
+
 	private bool thingFound = false;
 	private int birdsFound = 0;
 	private int likeFound = 0;
 	private int typesFound = 0;
 	private List<string> typesList = new List<string>();
+	private List<string> achievementsList = new List<string>();
 	private bool gameOver = false;
 	public bool isZoomed = false;
 
@@ -31,6 +46,7 @@ public class RaycastScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		achievementsScript = GameObject.Find("UICanvas");
 		zoom.SetActive(false);
 	}
 	
@@ -62,18 +78,34 @@ public class RaycastScript : MonoBehaviour {
 							
 						if (birdsFound == 1) {
 							firstBirdFound = true; // if this is the first bird found, unlock the achievement
+							if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().firstBirdFoundText)) {
+								achievementsList.Add(achievementsScript.GetComponent<Achievements>().firstBirdFoundText);
+								displayFirstBirdFound = true;
+							}
 							Debug.Log("Achievement! Found 1 Bird");
 						}
 						else if (birdsFound == 5) {
 							fiveBirdsFound = true; // likewise for the fifth...
+							if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().fiveBirdsFoundText)) {
+								achievementsList.Add(achievementsScript.GetComponent<Achievements>().fiveBirdsFoundText);
+								displayFiveBirdsFound = true;
+							}
 							Debug.Log("Achievement! Found 5 Birds");
 						}
 						else if (birdsFound == 10) {
 							tenBirdsFound = true; // ...and tenth...
+							if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().tenBirdsFoundText)) {
+								achievementsList.Add(achievementsScript.GetComponent<Achievements>().tenBirdsFoundText);
+								displayTenBirdsFound = true;
+							}
 							Debug.Log("Achievement! Found 10 Birds");
 						}
 						else if (birdsFound == 15) {
 							fifteenBirdsFound = true; // ...and fifteenth birds found
+							if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().fifteenBirdsFoundText)) {
+								achievementsList.Add(achievementsScript.GetComponent<Achievements>().fiveBirdsFoundText);
+								displayFifteenBirdsFound = true;
+							}
 							Debug.Log("Achievement! Found 15 Birds");
 						}
 
@@ -84,6 +116,10 @@ public class RaycastScript : MonoBehaviour {
 								
 							if (typesFound == 3) {
 								threeTypesFound = true; // if the player has found three different types of birds, unlock the achievement
+								if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().threeTypesFoundText)) {
+									achievementsList.Add(achievementsScript.GetComponent<Achievements>().threeTypesFoundText);
+									displayThreeTypesFound = true;
+								}
 								Debug.Log("Achievement! Found 3 Different Species");
 							}
 						}
@@ -92,6 +128,10 @@ public class RaycastScript : MonoBehaviour {
 								
 							if (likeFound == 3) {
 								threeLikeBirdsFound = true; // ...and if that number is three, unlock the achievement
+								if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().threeLikeBirdsFoundText)) {
+									achievementsList.Add(achievementsScript.GetComponent<Achievements>().threeLikeBirdsFoundText);
+									displayThreeLikeBirdsFound = true;
+								}
 								Debug.Log("Achievement! Birds of a Feather");
 							}
 						}
@@ -100,23 +140,43 @@ public class RaycastScript : MonoBehaviour {
 				else if (hit.transform.tag == "rare") {
 					birdsFound++;
 					rareBirdFound = true; // if the player finds a rare bird, unlock the achievement
+					if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().rareBirdFoundText)) {
+						achievementsList.Add(achievementsScript.GetComponent<Achievements>().rareBirdFoundText);
+						displayRareBirdFound = true;
+					}
 					Debug.Log("Achievement! Found a Rare Bird");
 				}
 				else if (hit.transform.tag == "ufo") {
 					ufoFound = true; // if the player finds the UFO, unlock the achievement
+					if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().ufoFoundText)) {
+						achievementsList.Add(achievementsScript.GetComponent<Achievements>().ufoFoundText);
+						displayUfoFound = true;
+					}
 					Debug.Log("Achievement! Close Encounter");
 				}
 				else if (hit.transform.tag == "bug") {
 					bugFound = true; // likewise for the bug
+					if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().bugFoundText)) {
+						achievementsList.Add(achievementsScript.GetComponent<Achievements>().bugFoundText);
+						displayBugFound = true;
+					}
 					Debug.Log("Achievement! Debugging");
 				}
 				else if (hit.transform.tag == "sun") {
 					sunFound = true;
+					if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().sunFoundText)) {
+						achievementsList.Add(achievementsScript.GetComponent<Achievements>().sunFoundText);
+						displaySunFound = true;
+					}
 					Debug.Log("Achievement! Sungazing");
 				}
 
 				if (gameOver == true && thingFound == false) {
 					nothingFound = true; // if the player found nothing during the whole session, unlock the achievement
+					if (!achievementsList.Contains(achievementsScript.GetComponent<Achievements>().nothingFoundText)) {
+						achievementsList.Add(achievementsScript.GetComponent<Achievements>().nothingFoundText);
+						displayNothingFound = true;
+					}
 					Debug.Log("Achievement! Missing the Point");
 				}
 			} // end achievement check
